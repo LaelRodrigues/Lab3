ec308326d2b78160ba60abc70e74725293d9b4fb
LaelRodrigues <laelrodrigues7@gmail.com>
2017-10-26 21:13:42 -0300

    Adicionando os arquivos bebida.h e bebida.cpp

---
+++ include/questao1/bebida.h
@@ -0,0 +1,51 @@
+/**
+ * @file	bebida.h
+ * @brief	Definicao da classe Bebida para representar uma bebida
+ * @author	Lael Rodrigues(laelrodrigues7@gmail.com)
+ * @since	25/10/2017
+ * @data	25/10/2017
+ */
+
+#ifndef _BEBIDA_H_
+#define _BEBIDA_H_
+
+#include "produto.h"
+
+/**
+ * @class 	Bebida bebida.h
+ * @brief	Classe que representa uma bebida
+ * @details Atributos da bebida: codigo, descricao,
+ *			preco e teor alcoolico
+ */
+
+class Bebida : public Produto
+{
+public:
+
+	/** @brief Construtor padrao */
+	Bebida();
+
+	/** @brief Construtor parametrizado */
+	Bebida(std::string _codigo, std::string _descricao, short _preco, 
+			int _teor_alcoolico);
+
+	/** @brief Destrutor padrao*/
+	~Bebida();
+
+private:
+	int teor_alcoolico;	/**< Teor alcoolico da bebida */
+
+public:
+	/** @brief Retorna o teor alcoolico da bebida */
+	int getTeorAlcoolico();
+
+	/** @brief Modifica o teor alcoolico da bebida */
+	void setTeorAlcoolico(int _teor_alcoolico);
+
+private:
+	/** @brief Metodo para imprimir os dados de uma bebida */
+	std::ostream& print(std::ostream &o) const;
+
+};
+
+#endif
---
+++ src/questao1/bebida.cpp
@@ -0,0 +1,52 @@
+/**
+ * @file	bebida.cpp
+ * @brief	Implemencao dos metodos da classe Bebida
+ *			para representar uma bebida
+ * @author	Lael Rodrigues(laelrodrigues7@gmail.com)
+ * @since	25/10/2017
+ * @date	25/10/2017
+ */
+
+#include <iomanip>
+#include "bebida.h"
+
+//Construtor padrao
+Bebida::Bebida() {}
+
+/**
+ * @param _codigo Codigo da bebida
+ * @param _descricao Descricao da bebida 
+ * @param _preco Preco da bebida
+ * @param _teor_alcoolico Teor alcoolico da bebida  
+ */
+Bebida::Bebida(std::string _codigo, std::string _descricao, short _preco, 
+	int _teor_alcoolico):
+	Produto(_codigo, _descricao, _preco), teor_alcoolico(_teor_alcoolico) {}
+
+//Destrutor padrao
+Bebida::~Bebida() {}
+
+/** @return Teor alcoolico da bebida */
+int
+Bebida::getTeorAlcoolico() {
+	return teor_alcoolico;
+}
+
+/** @param _teor_alcoolico Teor alcoolico da bebida */
+void
+Bebida::setTeorAlcoolico(int _teor_alcoolico) {
+	teor_alcoolico = _teor_alcoolico;
+}
+
+/** 
+ * @param o Referencia para stream de saida
+ * @return Referencia para Stream de saida
+ */
+std::ostream& 
+Bebida::print(std::ostream &o) const {
+	o << std::setfill (' ') << std::setw (10) << m_cod_barras << " | " 
+		<< std::setfill ('.') << std::setw (20) << m_descricao << " | " 
+		<< std::setfill (' ') << std::setw (5) << m_preco << " | "
+		<< std::setfill (' ') << std::setw (3) << teor_alcoolico << "%"; 
+	return o;
+}
