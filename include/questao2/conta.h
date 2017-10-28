5d68af154b073138489058732ee589b4dc8c459f
LaelRodrigues <laelrodrigues7@gmail.com>
2017-10-28 20:20:42 -0300

    Adicionando os arquivos conta.h e conta.cpp

---
+++ include/questao2/conta.h
@@ -0,0 +1,105 @@
+/**
+ * @file	conta.h
+ * @brief	Definicao da classe Conta para representar uma Conta
+ * @author	Lael Rodrigues(laelrodrigues7@gmail.com)
+ * @since	28/10/2017
+ * @data	28/10/2017
+ */
+
+#ifndef CONTA_H
+#define CONTA_H
+
+#include <memory>
+using std::shared_ptr;
+
+#include <string>
+using std::string;
+
+#include <iostream>
+using std::ostream;
+
+#include <vector>
+using std::vector;
+
+#include "movimentacao.h"
+
+/**
+ * @class 	Conta conta.h
+ * @brief	Classe que representa uma Conta
+ * @details Atributos de uma conta: agencia, numero, saldo,
+ *			status, limite, limite disponivel e a lista de
+ *			movivementacaoes
+ */
+
+class Conta {
+	
+	private:
+		string agencia; 		/**< Numero da agencia */
+		string numero;			/**< Numero da conta */
+		float saldo;			/**< Saldo da conta */
+		bool status;			/**< status da conta(se e especial ou nao) */
+		float limite;			/**< Limite da conta(caso seja especial) */
+		float limiteDisp;		/**< Limite disponivel da conta(caso seja especial) */
+		vector<shared_ptr<Movimentacao>> movimentacoes;  /**< Lista de movimentacoes */
+
+	public:
+		/** @brief Construtor padrao */
+		Conta();
+
+		/** @brief Construtor parametrizado */
+		Conta(string _agencia, string _numero, float _saldo, bool _status, float _limite);
+
+		/** @brief Destrutor padrao */
+		~Conta();
+
+		/** @brief Retorna o numero da agencia */
+		string getAgencia();
+
+		/** @brief Retorna o numero da conta */
+		string getNumero();
+
+		/** @brief Retorna o saldo da conta */
+		float getSaldo();
+
+		/** @brief Retorna se a conta e especial ou nao */
+		bool getStatus();
+
+		/** @brief Retorna O limite da conta(se a conta for especial) */
+		float getLimite();
+
+		/** @brief Retorna O limite disponivel da conta(se a conta for especial) */
+		float getLimiteDisp();
+
+		/** @brief Retorna A lista de movimentacoes */
+		vector<shared_ptr<Movimentacao>>& getMovimentacoes();
+
+		/** @brief Modifica o numero da agencia */
+		void setAgencia(string _agencia);
+
+		/** @brief Modifica o numero da conta */
+		void setNumero(string _numero);
+
+		/** @brief Modifica o saldo da conta*/
+		void setSaldo(float _saldo);
+
+		/** @brief Modifica o status(se a conta e especial ou nao)*/
+		void setStatus(bool _status);
+
+		/** @brief Modifica O limite da conta(se a conta for especial) */
+		void setLimite(float _limite);
+
+		/** @brief Modifica O limite disponivel da conta(se a conta for especial) */
+		void setLimiteDisp(float _limiteDisp);
+
+		/** @brief Adiciona uma movimentacao a lista de movimentacoes */
+		void setMovimentacoes(shared_ptr<Movimentacao>& _movimentacao);
+
+		/** @brief Sobrecarga do operador de comparacao */
+		bool operator==(Conta& conta);
+
+		/** @brief Sobrecarga do operador de insercao em stream */
+		friend ostream& operator<<(ostream& o, Conta& conta);
+
+};
+
+#endif
---
+++ src/questao2/conta.cpp
@@ -0,0 +1,152 @@
+/**
+ * @file	conta.cpp
+ * @brief	Implemencao dos metodos da classe Conta
+ *			para representar um conta
+ * @author	Lael Rodrigues(laelrodrigues7@gmail.com)
+ * @since	28/10/2017
+ * @date	28/10/2017
+ */
+
+
+#include "conta.h"
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+#include <iomanip>
+
+//Construtor padrao
+Conta::Conta() {}
+
+//Destrutor padrao
+Conta::~Conta() {}
+
+/**
+ * @param _agencia Numero da agencia
+ * @param _numero Numero da conta
+ * @param _saldo Valor do saldo bancario
+ * @param _status Informa se a conta e especial ou nao
+ * @param _limite Limite da conta(se for especial)
+ */
+Conta::Conta(string _agencia, string _numero, float _saldo, bool _status, 
+	float _limite) {
+	agencia = _agencia;
+	numero = _numero;
+	saldo = _saldo;
+	status = _status;
+	limite = _limite;
+	limiteDisp = _limite;
+}
+
+/** @return Numero da agencia */
+string Conta::getAgencia() {
+	return agencia;
+}
+
+/** @return Numero da conta */
+string Conta::getNumero() {
+	return numero;
+}
+
+/** @return Saldo da conta */
+float Conta::getSaldo() {
+	return saldo;
+}
+
+/** @return Se e especial ou nao */
+bool Conta::getStatus() {
+	return status;
+}
+
+/** @return Limite da conta(se for especial) */
+float Conta::getLimite() {
+	return limite;
+}
+
+/** @return Limite disponivel(se for especial) */
+float Conta::getLimiteDisp() {
+	return limiteDisp;
+}
+
+/** @return um lista de movimentacoes */
+vector<shared_ptr<Movimentacao>>& Conta::getMovimentacoes() {
+	return movimentacoes;
+}
+
+/** @param _agencia Numero da agencia */	
+void Conta::setAgencia(string _agencia) {
+	agencia = _agencia;
+}
+
+/** @param _numero Numero da conta */	
+void Conta::setNumero(string _numero) {
+	numero = _numero;
+}
+
+/** @param _saldo Saldo da conta */	
+void Conta::setSaldo(float _saldo) {
+	saldo = _saldo;
+}
+
+/** @param _status Informa se a conta e especial ou nao */	
+void Conta::setStatus(bool _status) {
+	status = _status;
+}
+
+/** @param _limite Limite da conta(se for especial) */
+void Conta::setLimite(float _limite) {
+	limite = _limite;
+}
+
+/** @param _limiteDisp Limite Disponivel da conta(se for especial) */
+void Conta::setLimiteDisp(float _limiteDisp) {
+	limiteDisp = _limiteDisp;
+}
+
+/** @param _movimentacao Adiciona uma movimentacao a lista */
+void Conta::setMovimentacoes(shared_ptr<Movimentacao>& _movimentacao) {
+	movimentacoes.push_back(_movimentacao);
+}
+
+/** 
+ * @param p Referencia para um objeto conta
+ * @return True ou false 
+ */
+bool Conta::operator==(Conta& conta) {
+	if(numero == conta.numero) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+
+/** 
+ * @param o Referencia para stream de saida
+ * @param p Referencia para um o objeto Conta
+ * @return Referencia para stream de saida
+ */
+ostream& operator<<(ostream& o, Conta& conta) {
+	
+	int cont = 0;
+	for(auto i = conta.getMovimentacoes().begin(); i != conta.getMovimentacoes().end(); i++) {
+		if((**i).getIndicacao() == credito) {
+			o << std::setfill(' ') << std::setw (15) << (**i).getDescricao() << " | "
+				 << std::setfill(' ') << std::setw (10) << (**i).getValor() << " | "
+				 << "tipo: credito";
+				 cont++;
+		}
+		else if((**i).getIndicacao() == debito) {
+			o << std::setfill(' ') << std::setw (15) << (**i).getDescricao() << " | "
+				 << std::setfill(' ') << std::setw (10) << (**i).getValor() << " | "
+				 << "tipo: debito";
+				 cont++;
+		}
+	}
+	if(cont == 0) {
+		cout << "Ainda nao foi ocorreu movimentacoes nessa conta." << endl;
+	}
+	return o;
+}
