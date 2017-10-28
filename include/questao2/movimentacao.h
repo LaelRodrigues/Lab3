93d20ec1c9a5e10bfe510fb227c8e0481cfba7ee
LaelRodrigues <laelrodrigues7@gmail.com>
2017-10-28 13:30:20 -0300

    Adicionando os arquivos movimentacao.h e movimentacao.cpp

---
+++ include/questao2/movimentacao.h
@@ -0,0 +1,68 @@
+/**
+ * @file	movimentacao.h
+ * @brief	Definicao da classe Movimentacao para representar 
+ *			uma movimentacao
+ * @author	Lael Rodrigues(laelrodrigues7@gmail.com)
+ * @since	28/10/2017
+ * @data	28/10/2017
+ */
+
+#ifndef MOVIMENTACAO_H
+#define MOVIMENTACAO_H
+
+#include <string>
+using std::string;
+
+/**
+ * @enum enum.h
+ * @brief Tipo enumeracao que identifica se a
+ *		  e de credito ou debito
+ */ 
+enum Indicacao {
+	credito = 1, 	/**< Movimentacao do tipo credito */
+	debito, 		/**< Movimentacao do tipo debito */
+};
+
+
+/**
+ * @class 	Movimentacao movimentacao.h
+ * @brief	Classe que representa uma Movimentacao
+ * @details Atributos de uma movimentacao: descricao,
+ *			valor e indicacao
+ */
+class Movimentacao {
+	private:
+		string descricao;   	/**< Descricao da movimentacao */
+		float valor;			/**< Valor da movimentacao */
+		Indicacao indicacao;    /**< Indicacao da movimentacao */
+
+	public:
+		/** @brief Construtor padrao */
+		Movimentacao();
+
+		/** @brief Construtor parametrizado */
+		Movimentacao(string _descricao, int _valor, Indicacao _indicacao);
+
+		/** @brief Destrutor padrao */
+		~Movimentacao();
+
+		/** @brief Retorna a descricao da movimentacao */
+		string getDescricao();
+
+		/** @brief Retorna o valor da movimentacao */
+		float getValor();
+
+		/** @brief Retorna A indicacao da movimentacao */
+		Indicacao getIndicacao();
+
+		/** @brief Modifica A descricao da movimentacao */
+		void setDescricao(string _descricao);
+
+		/** @brief Modifica o Valor da movimentacao */
+		void setValor(int _valor);
+
+		/** @brief Modifica a Indicacao da movimentacao */
+		void setIndicacao(Indicacao _indicacao);
+};
+
+#endif
---
+++ src/questao2/movimentacao.cpp
@@ -0,0 +1,58 @@
+/**
+ * @file	movimentacao.cpp
+ * @brief	Implemencao dos metodos da classe Movimentacao
+ *			para representar uma movimentacao
+ * @author	Lael Rodrigues(laelrodrigues7@gmail.com)
+ * @since	28/10/2017
+ * @date	28/10/2017
+ */
+
+
+#include <movimentacao.h>
+
+//Construtor padrao
+Movimentacao::Movimentacao() {}
+
+//Destrutor padrao
+Movimentacao::~Movimentacao() {}
+
+/**
+ * @param _descricao Descricao da movimentacao
+ * @param _valor valor da movimentacao
+ * @param _indicacao tipo(credito ou debito) da movimentacao
+ */
+Movimentacao::Movimentacao(string _descricao, int _valor, Indicacao _indicacao) {
+	descricao = _descricao;
+	valor = _valor;
+	indicacao = _indicacao;
+}
+
+/** @return Descricao da movimentacao */
+string Movimentacao::getDescricao() {
+	return descricao;
+}
+
+/** @return Valor da movimentacao */
+float Movimentacao::getValor() {
+	return valor;
+}
+
+/** @return Tipo(credito ou debito) da movimentacao */
+Indicacao Movimentacao::getIndicacao() {
+	return indicacao;
+}
+
+/** @param _descricao Descricao da movimentacao */	
+void Movimentacao::setDescricao(string _descricao) {
+	descricao = _descricao;
+}
+
+/** @param _valor Valor da movimentacao */	
+void Movimentacao::setValor(int _valor) {
+	valor = _valor;
+}
+
+/** @param _indicacao Tipo(credito ou debito) da movimentacao */	
+void Movimentacao::setIndicacao(Indicacao _indicacao) {
+	indicacao =_indicacao;
+}
